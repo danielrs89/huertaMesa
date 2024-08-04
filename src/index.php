95b7bffd3180a31c82ac00a741ef940ee0048558
danie <dani89rosa@gmail.com>
2024-08-04 20:21:06 +0200

    animacion y comprobar conexion

--- src/index.php
+++ src/index.php
@@ -8,27 +8,89 @@
 </head>
 
 <body>
-    
-
-        <?php
 
 
-        //ANIMACION & CONEXION
-try {
-    require("index.html");
-} catch (\Throwable $th) {
-    throw $th;
-}
+    <?php
+    function checkInternetConnection()
+    {
+        $connected = @fsockopen("www.google.com", 80);
+        if ($connected) {
+            $is_conn = true;
+            fclose($connected);
+        } else {
+            $is_conn = false;
+        }
+        return $is_conn;
+    }
+
+    $internetConnection = checkInternetConnection();
+
+
+    //ANIMACION & CONEXION
+    try {
+        if ($internetConnection) {
+            echo "<div class='status'>";
+            echo "<div class='image-container'>";
+            echo "<img src='assets\img\gallery\authors.png' alt='Rotating Image' class='rotating-image' id='rotatingImage'>";
+            echo "</div>";
+            echo "</div>";
+        }
+         require("index.html");
+    } catch (\Throwable $th) {
+        throw $th;
+    }
 
 
 
 
 
-       
 
-        ?>
 
-    
+    ?>
+
+   
+
+    <style>
+        .image-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .rotating-image {
+            width: 200px;
+            height: 200px;
+            animation: rotate 4s linear infinite;
+            animation-play-state: paused;
+        }
+
+        @keyframes rotate {
+            from {
+                transform: rotate(0deg);
+            }
+
+            to {
+                transform: rotate(360deg);
+            }
+        }
+
+        .status {
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+    <script>
+        // Start the animation
+        const rotatingImage = document.getElementById('rotatingImage');
+        rotatingImage.style.animationPlayState = 'running';
+
+        // Stop the animation after 4 seconds
+        setTimeout(() => {
+            rotatingImage.style.animationPlayState = 'paused';
+        }, 4000);
+    </script>
+
 </body>
 
 </html>
